f820754c96c0b265a07dcc1b3d8a02a9c5a6a3de
shashank <shashank.r@kanilebettu.com>
2025-05-13 20:06:35 +0530

    Initial Setup

---
+++ app/src/main/java/com/rith/vibechekr/MainActivity.java
@@ -0,0 +1,60 @@
+package com.rith.vibechekr;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.databinding.DataBindingUtil;
+
+import com.rith.vibechekr.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding activityMainBinding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        activityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        initUi();
+        setupOnClickListeners();
+        setupOnDisplayObservers();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    private void initUi() {
+    }
+
+    private void setupOnClickListeners() {
+    }
+
+    private void setupOnDisplayObservers() {
+    }
+
+}
